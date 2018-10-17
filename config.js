ce73fc5287be6e4092291c9189e53c131efb67cc
Christopher Coté <ccote@thecitybase.com>
2018-10-17 16:41:17 -0500

    update urls and default backend

--- config.js
+++ config.js
@@ -1,30 +1,13 @@
 flowiumConfig = {
-    URL: "https://flowium.com",
+    URL: "https://flowium.ops.cityba.se",
     backends: {
-        'FlowiumSandbox': {
+        'CityBase': {
             type: "GitHub",
             URL: "https://api.github.com",
-            clientID: "d027578d9cca180f9e0e",
-            gatekeeperURL: "https://flowium.herokuapp.com",
-            repository: "sustrik/flowium-sandbox",
+            clientID: "bfa6648e886c4a614145",
+            gatekeeperURL: "https://gatekeeper.ops.cityba.se",
+            repository: "CityBase/OpsChecklists",
             processPath: "/",
-        },
-        'gitlab-test': {
-            type: "GitLab",
-            URL: "https://gitlab.com",
-            applicationID: "4dbe6da72b8954701424c1439519d1debe60211f7294547e111787b1e340544b",
-            repository: "sustrik/flowium-sandbox",
-            processPath: "/",
-            editable: true,
-        },
-        'gitea-test': {
-            type: "Gitea",
-            URL: "https://try.gitea.io",
-            applicationID: "",
-            repository: "flowium/flowium-sandbox",
-            processPath: "/",
-            token: "7c270a276f42c3631645224e01bada02891bf925",
-            editable: true,
-        },
+        }
     }
 }
